3fa1917f311e39067cc01d9599e8e2f1d7a524c5
Ryan Sheidow <ryan.sheidow@icloud.com>
2024-08-29 18:46:36 -0400

    Quick update

--- weather.js
+++ weather.js
@@ -1,36 +1,10 @@
+const apiUrl = `https://api.openweathermap.org/data/2.5/weather?zip=${zipCode}&units=imperial&appid=${apiKey}`;
+
 document.getElementById("getWeatherBtn").addEventListener("click", function () {
   const zipCode = document.getElementById("zipCode").value;
   const apiKey = `c659eaa0c8083d9298d08833d0d75258`; // Replace with your OpenWeatherMap API key
-  const apiUrl = `https://api.openweathermap.org/data/2.5/weather?zip=${zipCode}&units=imperial&appid=${apiKey}`;
   console.log("Hello World");
-  fetch(apiUrl)
-    .then((response) => response.json())
-    .then((data) => {
-      console.log(data); // Ensure this line is in your code
-
-      // To output the data directly into your HTML
-      document.getElementById("weatherData").innerHTML = `
-          <p>API Response Data: ${JSON.stringify(data, null, 2)}</p>
-      `;
-      if (data.cod === 200) {
-        const weatherDescription = data.weather[0].description;
-        const temperature = data.main.temp;
-        document.getElementById("weatherResult").innerHTML = `
-                  <h3>Weather for ${data.name}</h3>
-                  <p>${weatherDescription}</p>
-                  <p>Temperature: ${temperature}°F</p>
-              `;
-      } else {
-        console.log(data); // Log the full response for debugging
-        document.getElementById("weatherResult").innerHTML =
-          `<p>Weather data not found.</p>`;
-      }
-    })
-    .catch((error) => {
-      console.error("Errors:", error); // Log the error to console
-      document.getElementById("weatherResult").innerHTML =
-        `<p>Error fetching data.</p>`;
-    });
+  getWeather(zipCode, apiKey);
 });
 
 document.addEventListener("DOMContentLoaded", function () {
@@ -61,8 +35,6 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 function getWeather(zipCode, apiKey) {
-  const apiUrl = `https://api.openweathermap.org/data/2.5/weather?zip=${zipCode}&units=imperial&appid=${apiKey}`;
-
   fetch(apiUrl)
     .then((response) => response.json())
     .then((data) => {
